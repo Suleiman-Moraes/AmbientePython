015bffbfdb435800b9465a408216eeb47a04277d
Suleiman Moraes <suleimanmoraes@gmail.com>
2018-12-05 16:40:22 -0200

    teste

---
+++ ambiente_python/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\areatrabalho\projetos\pycharm\ambientepython\ambiente_python\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
